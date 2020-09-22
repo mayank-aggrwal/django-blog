3cef6bffcea6e97c7fb9c5e6d0779c0c0593e002
mayank-aggrwal <aggarwal.mayank49@gmail.com>
2020-09-22 23:46:01 +0530

    Template tags to display data from DB to frontend

--- djangolog/articles/models.py
+++ djangolog/articles/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length = 100)
-    slug = models.SlugField
+    slug = models.SlugField(default='slug')
     body = models.TextField()
     date = models.DateTimeField(auto_now_add = True)
 
--- djangolog/articles/views.py
+++ djangolog/articles/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 def articles_list(request):
-    return render(request, 'articles/articles_list.html')
+    articles = Article.objects.all()
+    return render(request, 'articles/articles_list.html', { 'articles': articles })
