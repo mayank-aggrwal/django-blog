02be0fcb143db6034b5571a4d0519302fd45fa75
mayank-aggrwal <aggarwal.mayank49@gmail.com>
2020-09-24 00:40:05 +0530

    Add Pillow package, upload media

--- djangolog/djangolog/urls.py
+++ djangolog/djangolog/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +29,4 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
