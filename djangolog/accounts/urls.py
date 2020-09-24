a4acde38d88f4f04f4b8f98a7f674a485100ae17
mayank-aggrwal <aggarwal.mayank49@gmail.com>
2020-09-24 19:27:04 +0530

    Functionality to log the users in

--- djangolog/accounts/urls.py
+++ djangolog/accounts/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import url
 from django.urls import path
 from . import views
 
+app_name = 'accounts'
+
 urlpatterns = [
     path('signup/', views.signup_page),
+    path('login/', views.login_page, name='login-page'),
 ]
--- djangolog/accounts/views.py
+++ djangolog/accounts/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def signup_page(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
             return redirect('articles:list')
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', { 'form': form })
+
+def login_page(request):
+    if request.method == 'POST':
+        print('login POST request received:', request.POST)
+        form = AuthenticationForm(data=request.POST)
+        print(form)
+        print('login POST request received:', form.get_user())
+        if form.is_valid():
+            print('form is valid')
+            user = form.get_user()
+            login(request, user)
+            print('redirecting')
+            return redirect('articles:list')
+        else:
+            print('form is invalid')
+    else:
+        print('login GET request received')
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', { 'form': form })
