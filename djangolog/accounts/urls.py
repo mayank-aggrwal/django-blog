024f8756b80a1522a1ff408691cbc544f80ad8ec
mayank-aggrwal <aggarwal.mayank49@gmail.com>
2020-09-25 16:07:54 +0530

    Conditionally render web page on the basis of login status of user

--- djangolog/accounts/urls.py
+++ djangolog/accounts/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', views.signup_page),
+    path('signup/', views.signup_page, name='signup-page'),
     path('login/', views.login_page, name='login-page'),
     path('logout/', views.logout_page, name='logout'),
 ]
