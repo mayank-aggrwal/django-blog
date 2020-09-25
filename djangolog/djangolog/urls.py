fd68a8a3fcd1b8015e16fe27cd58376f0df01303
mayank-aggrwal <aggarwal.mayank49@gmail.com>
2020-09-25 15:51:01 +0530

    Redirect base url to main articles page

--- djangolog/djangolog/urls.py
+++ djangolog/djangolog/urls.py
@@ -19,6 +19,7 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from articles import views as article_views
 
 
 urlpatterns = [
@@ -26,7 +27,7 @@ urlpatterns = [
     path('articles/', include('articles.urls')),
     path('accounts/', include('accounts.urls')),
     path('about/', views.about),
-    path('', views.homepage),
+    path('', article_views.articles_list, name='home'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
