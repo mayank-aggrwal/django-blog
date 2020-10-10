c5c4d80df2000ab8a93d796c335d3a8e9d2c9eb6
mayank-aggrwal <aggarwal.mayank49@gmail.com>
2020-10-11 01:41:31 +0530

    Send POST requests through fetch()

--- djangolog/assets/index.js
+++ djangolog/assets/index.js
@@ -7,20 +7,53 @@ errors = document.getElementById('errors')
 const shortenerForm = document.getElementById('shortener-form')
 
 
-function shortenURL(e) {
+async function shortenURL(e) {
     e.preventDefault()
     urlToShorten = url.value
     customCode = code.value
-    if(customCode == '') {
-        customCode = 'Not applicable'
+    // if(customCode == '') {
+    //     customCode = 'Not applicable'
+    // }
+    console.log(`URL: ${urlToShorten}\nCode: ${customCode}`);
+
+    let shortenedURL = ':)'
+
+    let requestURL = "http://localhost:5000/api/custom"
+    let requestBody = {
+        longUrl: urlToShorten,
+        urlCode: customCode
     }
-    console.log(`URL: ${urlToShorten}\nCode: ${customCode}`)
-
+    if(customCode == '') {
+        requestURL = "http://localhost:5000/api/short"
+        requestBody = {
+            longUrl: urlToShorten
+        }
+    }
+    console.log(customCode)
     // Make an API call to the URL shortening service and get the json and display the shortened URL or errors(if any)
-    shortenedURL = ':)'
-    const displayText = 'Your shortened URL is : ' + shortenedURL
-    result.innerHTML = displayText
+    // await fetch("https://shortt-ly.herokuapp.com/api/short", {
+    await fetch(requestURL, {
+        method: 'POST',
+        body: JSON.stringify(requestBody),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    }).then(response => response.json())
+      .then(json => {
+            console.log(json)
+            if(json.hasOwnProperty('errors')) {
+                let displayText = `Error: ${json.errors.message}`
+                result.innerHTML = ''
+                errors.innerHTML = displayText
+            }
+            else {
+                shortenedURL = json.shortUrl;
+                let displayText = 'Your shortened URL is : ' + shortenedURL
+                errors.innerHTML = ''
+                result.innerHTML = displayText
+            }
+      })
+      .catch(err => console.log(err));
 }
-
 
 shortenerForm.addEventListener('submit', shortenURL)
